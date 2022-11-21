38c60e9fc9f0d0e46ed473a8c2dff3e672584621
Zynh0722 <Zynh0722@gmail.com>
2022-11-20 18:26:38 -0800

    wavyhandauto

--- src/main/java/frc/robot/commands/auto/WavyHand.java
+++ src/main/java/frc/robot/commands/auto/WavyHand.java
@@ -4,25 +4,41 @@
 
 package frc.robot.commands.auto;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
 
 public class WavyHand extends CommandBase {
+  Drivetrain drivetrain;
+
   /** Creates a new WavyHand. */
-  public WavyHand() {
+  public WavyHand(Drivetrain drivetrain) {
+    this.drivetrain = drivetrain;
+
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(this.drivetrain);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    SmartDashboard.putBoolean("auto_state", true);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    Double fwd = SmartDashboard.getNumber("fwd_scalar", 0);
+    Double rot = SmartDashboard.getNumber("rot_scalar", 0);
+
+    drivetrain.drive(fwd, rot);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    SmartDashboard.putBoolean("auto_state", false);
+  }
 
   // Returns true when the command should end.
   @Override
