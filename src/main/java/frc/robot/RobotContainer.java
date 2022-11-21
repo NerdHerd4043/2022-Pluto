e3199792b93892bfb7312dade07b5f8233638310
Zynh0722 <Zynh0722@gmail.com>
2022-11-20 19:09:21 -0800

    reverse reverse

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.auto.WavyHand;
 import frc.robot.commands.hatchlatch.Clap;
 import frc.robot.subsystems.CargoIntake;
 import frc.robot.subsystems.Drivetrain;
@@ -29,8 +28,6 @@ public class RobotContainer {
   private final HatchLatch hatchLatch = new HatchLatch();
 
   private static XboxController driveStick = new XboxController(0);
-
-  private final WavyHand wavyHandAuto = new WavyHand(drivetrain);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -80,6 +77,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return wavyHandAuto;
+    return null;
   }
 }
