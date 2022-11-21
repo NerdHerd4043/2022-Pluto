9b22a05d0051a319615e4523074732ffcf8c4f29
Zynh0722 <Zynh0722@gmail.com>
2022-11-20 18:28:22 -0800

    add autocommand to robot.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.auto.WavyHand;
 import frc.robot.commands.hatchlatch.Clap;
 import frc.robot.subsystems.CargoIntake;
 import frc.robot.subsystems.Drivetrain;
@@ -28,6 +29,8 @@ public class RobotContainer {
   private final HatchLatch hatchLatch = new HatchLatch();
 
   private static XboxController driveStick = new XboxController(0);
+
+  private final WavyHand wavyHandAuto = new WavyHand(drivetrain);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -77,6 +80,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return null;
+    return wavyHandAuto;
   }
 }
